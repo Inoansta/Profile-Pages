ed912f1518d024d2e3bf23a1c759d308e9a86174
Paul Yang <sea5122916@gmail.com>
2023-06-07 22:35:10 +0900

    Mongo DB Schema Setting up
    have added the parameters

---
+++ Profiles Page/profiles/src/Model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+const profileSchema = new Schema({
+name: String,
+email: String,
+phone: String
+})
+
+const Profiles = model('Profiles', profileSchema);
+
+module.exports = Profiles; 
--- Profiles Page/profiles/src/Server.js
+++ Profiles Page/profiles/src/Server.js
@@ -1,11 +1,17 @@
+const Profiles = require('./Model')
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGOOSE_URI).then(()=>{console.log('MongoDB Connected Well')})
 
+app.get('/save', (req, res)=>{
+    let new_profile = Profiles(req);
+    new_profile.save()
+})
+
 app.listen(3000, ()=> {
     console.log('Server Started')});
-
 
