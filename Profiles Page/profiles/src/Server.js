a934e85050923f0d239ed4084b5e2e31cf0c757f
Paul Yang <sea5122916@gmail.com>
2023-06-12 23:53:22 +0900

    working on Gridfs again
    done with uploading file and metadata with gridfs
    
    working on retrieving files through gridfs

--- Profiles Page/profiles/src/Server.js
+++ Profiles Page/profiles/src/Server.js
@@ -1,57 +1,101 @@
-const Profiles = require('./Model')
+const path = require('path');
 const express = require('express');
 const app = express();
+const multer = require('multer');
+const Grid = require('gridfs-stream');
+const {GridFsStorage} = require('multer-gridfs-storage');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const crypto = require('crypto')
 
 require('dotenv').config();
-
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGOOSE_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: 'Profile'
-})
-.then(console.log('DB connected!'))
+const conn = mongoose.createConnection(process.env.MONGOOSE_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+//Initialize GridFS
+let gfs;
+conn.once('open', () => {
+  console.log('GridFS initialized!')
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');  
+  
+});
+
+// Create storage engine for Multer
+const storage = new GridFsStorage({
+  url: process.env.MONGOOSE_URI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+          
+          
+
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads',
+          metadata:{
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone
+          }
+        };
+        resolve(fileInfo);
+      });
+    });
+  
+}});
+const upload = multer({ storage });
+
 
 //uploaindg the datas
-app.post('/data', (req, res)=>{
-    console.log(req.body);
-
-    const newData = new Profiles(req.body)  ;
-    newData.save()
-    .then(() => console.log('Data saved successfully!'))
-    .catch((err) =>   
-    {console.log('Error saving data:', err)``
-    res.status(500).json({ error: err.message })});
-    
+app.post('/data', upload.single('file'),(req, res)=>{    
+    console.log('Sent successfully!')
     res.redirect('/')
 })
 
 
-//retreiving the data
+//retreiving the datas
 app.get('/data', (req, res)=>{
-  const { filename } = req.params;
-
-    Profiles.find({}, (err, data)=>{
-      if (err) {
-        res.status(500).json({ error: err.message });
-      } else {
-        res.json(data);
-      }
-    });
+  gfs.collection('uploads').find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      res.render('index', { files: false });
+    } else {
+      files.map(file => {
+        if (
+          file.contentType === 'image/jpeg' ||
+          file.contentType === 'image/png'
+        ) {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+      res.render('index', { files: files });
+    }
+  });
 })
 
 
-//delete the data
+//delete the data by its _id
 app.delete('/data', (req, res)=>{
-  
-})
+ 
+    res.redirect('/')
+})  
 
 
-//edit the data
+//edit the data and call it by its _id
 app.put('/data', (req, res)=>{
+  
 
 })
 
@@ -59,4 +103,3 @@ app.put('/data', (req, res)=>{
 
 app.listen(3000, ()=> {
     console.log('Server Started')})
-
