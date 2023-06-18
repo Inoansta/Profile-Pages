9c9a930b90c5fa022a938d19f337f7ae226d7096
Paul Yang <sea5122916@gmail.com>
2023-06-18 20:32:54 +0900

    working on GridFS again
    different way to uploading is working deleting is not working yet

--- Profiles Page/profiles/src/Server.js
+++ Profiles Page/profiles/src/Server.js
@@ -4,7 +4,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const multer = require('multer');
-const {GridFsStorage} = require('multer-gridfs-storage');
+// const {GridFsStorage} = require('multer-gridfs-storage');
+const {createModel} = require('mongoose-gridfs');
+const { ObjectId } = require('mongodb');
+const { Readable } = require('stream');
 const bodyParser = require('body-parser');
 
 require('dotenv').config();
@@ -13,66 +16,87 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+let Attachment
 //connecting MongoDB
-const conn = mongoose.createConnection(process.env.MONGOOSE_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const conn = mongoose.connect(process.env.MONGOOSE_URI, {
+  useNewUrlParser: true,
+}).then(()=> {
+  console.log("Connected to MongoDB");
+  // Allocate model into Attachment
+  Attachment = createModel();
+}).catch((error) => {
+  console.error(error);
+});
 
 
 //Initialize GridFS
-let gfs;
-conn.once('open', () => {
-  console.log('GridFS initialized!')
-  gfs =  new mongoose.mongo.GridFSBucket(conn.db, {
-    bucketName: "pures"
-  });  
+// let gfs;
+// conn.once('open', () => {
+//   console.log('GridFS initialized!')
+//   gfs =  new mongoose.mongo.GridFSBucket(conn.db, {
+//     bucketName: "pures"
+//   });  
   
   
-});
+// });
 
-// Create storage engine for Multer
-const storage = new GridFsStorage({
-  url: process.env.MONGOOSE_URI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
+// // Create storage engine for Multer
+// const storage = new GridFsStorage({
+//   url: process.env.MONGOOSE_URI,
+//   file: (req, file) => {
+//     return new Promise((resolve, reject) => {
+//       crypto.randomBytes(16, (err, buf) => {
+//         if (err) {
+//           return reject(err);
+//         }
 
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketnName: "pures",
-          metadata:{
-            name: req.body.name,
-            email: req.body.email,
-            phone: req.body.phone
-          }
-        };
-        resolve(fileInfo);
-      });
-    });
+//         const filename = buf.toString('hex') + path.extname(file.originalname);
+//         const fileInfo = {
+//           filename: filename,
+//           bucketnName: "pures",
+//           metadata:{
+//             name: req.body.name,
+//             email: req.body.email,
+//             phone: req.body.phone
+//           }
+//         };
+//         resolve(fileInfo);
+//       });
+//     });
   
-}});
-const upload = multer({ storage });
+// }});
+// const upload = multer({ storage });
 
 
 //uploaindg the datas
-app.post('/data', upload.single("file"),(req, res)=>{    
-    console.log('Sent successfully!')
+app.post('/data', (req, res)=>{    
+  const storage = multer.memoryStorage()
+  const upload = multer({ storage: storage})
+  upload.single('file')(req, res, (err) => {
+    if (err) {
+        return res.status(400).json({ message: "Upload Request Validation Failed" });
+    } else if(!req.body.name) {
+        return res.status(400).json({ message: "No track name in request body" });
+    }
+
+    const readStream = Readable.from(req.file.buffer);
+    const options = ({ filename: req.file.originalname, contenttype: "image/png", metadata:{name: req.body.name, email: req.body.email, phone: req.body.phone}});
+    Attachment.write(options, readStream)
     res.redirect('/')
-})
+});
+});
 
 
 //retreiving the datas
-app.get('/data', async (req, res)=>{
-  gfs =  new mongoose.mongo.GridFSBucket(conn.db, {
-    bucketName: "pures"
-  });
-  const cursor = gfs.find();
-  for await (const doc of cursor){
-    console.log(doc)
-  }
-  res.redirect("/")
+app.get('/data', (req, res)=>{
+  // gfs =  new mongoose.mongo.GridFSBucket(conn.db, {
+  //   bucketName: "pures"
+  // });
+  // const cursor = gfs.find();
+  // for await (const doc of cursor){
+  //   console.log(doc)
+  // }
+  // res.redirect("/")
   // gfs.find().toArray((err, files) => {
     
   //   // Check if files
@@ -83,13 +107,23 @@ app.get('/data', async (req, res)=>{
   //   } 
   //   return res.json(files);
   // });
+  res.set('content-type', "image/png");
+  res.set('accept-ranges', 'bytes');
+
+
 })
 
 
 //delete the data by its _id
 app.delete('/data', (req, res)=>{
- 
-    res.redirect('/')
+  if(!req.query.id) {
+    return res.status(400).json({
+        message: "Invalid ID in URL parameter."
+    });
+}
+// const id = new ObjectId(req.query.id)
+Attachment.unlink(req.query.id);
+
 })  
 
 
@@ -103,6 +137,3 @@ app.put('/data', (req, res)=>{
 
 app.listen(3000, ()=> {
     console.log('Server Started')})
-
-
-module.export = app;
